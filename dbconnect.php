fb8d75df5f82e59c06b968aebc130375dd7461c0
Muadh <imuatth@gmail.com>
2020-12-15 12:13:40 +0400

    edit db connect

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-$db = "lis";
-$user = "root";
-$pass = "";
-$hostname = "localhost";
+$db = "AsoaDPrpXL";
+$user = "AsoaDPrpXL";
+$pass = "J3fFq9a4Ho";
+$hostname = "remotemysql.com";
 $conn = mysqli_connect($hostname, $user, "$pass", $db)
     or die("Unable to connect to database");
 ?>
